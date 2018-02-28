58f367178476c0e9c5a8abba40cf9ce3387caeb3
pankona <yosuke.akatsuka@gmail.com>
2018-02-28 14:46:10 +0900

    example of json unmarshal

--- src/main/kotlin/io/pankona/main.kt
+++ src/main/kotlin/io/pankona/main.kt
@@ -1,5 +1,18 @@
 package io.pankona
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+data class LaunchRequest(val launch: Array<launchURL>)
+data class launchURL(val launchUrl: String, val appType: String)
+
 fun main(args: Array<String>) {
-    println("Hello World!!")
+    val json = "{\"launch\":[{\"launchUrl\":\"http://fraunhoferfokus.github.io/node-hbbtv/www/cs-app.html?app2appURL=ws://192.168.0.106:8080/remote/\",\"appType\":\"html\"}]}"
+    val mapper = jacksonObjectMapper()
+
+    val request = mapper.readValue<LaunchRequest>(json)
+
+    println(request)
+    println(request.launch[0])
+    println(request.launch[0].launchUrl)
 }
